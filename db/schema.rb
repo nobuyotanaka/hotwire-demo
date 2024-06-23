5c4fdd25c6c8bffc993e0364112d205cfd3e2495
nobuyotanaka <kshiris1028@gmail.com>
2024-06-23 16:14:48 +0900

    犬のscaffoldを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_23_063328) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_23_071309) do
   create_table "cats", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "dogs", force: :cascade do |t|
     t.string "name"
     t.integer "age"
     t.datetime "created_at", null: false
